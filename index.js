8747f11a997af50b76955d836967a12258d9163e
ankitdas13 <ankit.das@razorpay.com>
2022-09-28 13:20:01 +0530

    changes 1

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require('express')
+const Razorpay = require("razorpay")
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', async function(request, response) {
+    var instance = new Razorpay({
+     key_id: 'rzp_test_xMpBchciV4VQSE',
+     key_secret: 'ecwPEz6hRmTTMapOx733NLEB',
+    });
+
+    var orders = await instance.orders.all();  
+  response.send(orders);
 })
 
 app.listen(app.get('port'), function() {
